4a32519dc035be2bc23ca9b0de799fc8a2bbe785
Keean Ferreira <keeanferreira07@gmail.com>
2023-08-16 10:01:22 +0200

    More edits to following camera
    More edits to following camera

--- Assets/_Scripts/CameraManager.cs
+++ Assets/_Scripts/CameraManager.cs
@@ -10,6 +10,7 @@ public class CameraManager : MonoBehaviour
     private Vector3 relativePosition;
     private float halfScreenHeight;
     private float halfScreenWidth;
+    private float fps;
 
     // Start is called before the first frame update
     void Start()
@@ -22,28 +23,32 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(fps = (int)(1f / Time.unscaledDeltaTime));
         relativePosition = cam.transform.InverseTransformDirection(player.transform.position - cam.transform.position);
-        Debug.Log(relativePosition);
+        //Debug.Log(new Vector2(halfScreenWidth - relativePosition.x, halfScreenHeight - relativePosition.y));
+        //Debug.Log(relativePosition);
         //cam.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
 
-        if ((halfScreenHeight - relativePosition.x < 10.0f) && (halfScreenHeight - relativePosition.x < 5.0f))
-        {
-            cam.transform.Translate(1f, 0f, 0f);
-        }
+        cam.transform.position = new Vector3(cam.transform.position.x, player.transform.position.y, -10f);
+
+        //if (halfScreenHeight - relativePosition.y < 4.0f)
+        //{
+        //    cam.transform.Translate(0f, 0.05f, 0f);
+        //}
         
-        if ((halfScreenHeight - relativePosition.x > -10.0f) && (halfScreenHeight - relativePosition.x < -5.0f))
-        {
-            cam.transform.Translate(-1f, 0f, 0f);
-        }
+        //if ((halfScreenHeight - relativePosition.y < 10.0f) && (halfScreenHeight - relativePosition.y > 6.0f))
+        //{
+        //    cam.transform.Translate(0f, -0.05f, 0f);
+        //}
         
-        if ((halfScreenWidth - relativePosition.y < 10.0f) && (halfScreenWidth - relativePosition.y < 5.0f))
-        {
-            cam.transform.Translate(0f, 0.01f, 0f);
-        }
+        //if (halfScreenWidth - relativePosition.x < 5.0f)
+        //{
+        //    cam.transform.Translate(0.01f, 0f, 0f);
+        //}
         
-        if ((halfScreenWidth - relativePosition.y > -10.0f) && (halfScreenWidth - relativePosition.y < -5.0f))
-        {
-            cam.transform.Translate(0f, 0.01f, 0f);
-        }
+        //if (halfScreenWidth - relativePosition.x > 5.0f)
+        //{
+        //    cam.transform.Translate(0.01f, 0.01f, 0f);
+        //}
     }
 }
