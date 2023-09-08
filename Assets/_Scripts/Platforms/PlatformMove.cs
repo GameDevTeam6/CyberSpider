68028d244cc1bd72dd9a2b6b3c0100fde0c91615
Keean Ferreira <keeanferreira07@gmail.com>
2023-09-08 19:46:04 +0200

    Busy implementing moving platforms

---
+++ Assets/_Scripts/Platforms/PlatformMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    [SerializeField] float moveDistance;
+    [SerializeField] float moveSpeed;
+    private Vector3 initialPos;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < initialPos.x + moveDistance)
+        {
+            //rb.AddForce(new Vector3(moveSpeed, 0f, 0f));
+            transform.Translate(new Vector3(moveSpeed, 0f, 0f));
+        } else if (transform.position.x < initialPos.x - moveDistance)
+        {
+            transform.Translate(new Vector3(-moveSpeed, 0f, 0f));
+            //rb.AddForce(new Vector3(-moveSpeed, 0f, 0f));
+        }
+    }
+
+    private void MoveLeft()
+    {
+
+    }
+}
