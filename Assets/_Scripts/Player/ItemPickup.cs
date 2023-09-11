949c328904bab66e794cc728223bb21c2f8824db
MariyaShavshy <mariyashavshy@protonmail.com>
2023-09-10 17:27:37 -0700

    refined pickup item fixing

--- Assets/_Scripts/Player/ItemPickup.cs
+++ Assets/_Scripts/Player/ItemPickup.cs
@@ -25,7 +25,6 @@ public class ItemPickup : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("ItemPickup::OnTrigger, collided with:" + col.gameObject.tag);
 
         // collision with pickup items
         if (col.gameObject.CompareTag("Pickup"))
@@ -35,18 +34,17 @@ public class ItemPickup : MonoBehaviour
 
             ////////////////// BITCOIN TOKEN ///////////////////
 
-            Debug.Log("PlayerController::OnCollision, picked up:" + itemType);
-
             if (itemType == ActionType.Score)
             {
                 // fetch reward value to be added to the score
                 float val = col.gameObject.GetComponent<ItemInfo>().item.actionValue;
                 // update score
                 _score = gameObject.GetComponent<PlayerStats>().ChangeScore(val);
+                Destroy(col.gameObject);
             }
 
             ////////////////// HEALTH TOKEN ///////////////////
-            if (itemType == ActionType.Health)
+            else if (itemType == ActionType.Health)
             {
                 // fetch current health
                 float currentHealth = gameObject.GetComponent<PlayerStats>().GetHealth();
@@ -64,42 +62,22 @@ public class ItemPickup : MonoBehaviour
                     float diff = 100 - currentHealth;
                     _health = gameObject.GetComponent<PlayerStats>().ChangeHealth(diff);
                 }
-            }
-        }
 
-        // if player collides with pickup items
-        if (col.gameObject.CompareTag("Pickup"))
-        {
-          //editor.OpenQuestionPanel(() => {
-          //});
-
-            Item item = col.gameObject.GetComponent<ItemInfo>().item;
-            if (item.actionType == ActionType.Score)
-            {
-                // delete token from scene
-                Debug.Log("ItemPickup::OnTrigger, score item detected");
-                Debug.Log(PlayerStats.finalScore);
                 Destroy(col.gameObject);
             }
-            // if the pickup item is a health token
-            else if (item.actionType == ActionType.Health)
-            {
-              // delete token from scene
-              Destroy(col.gameObject);
-            }
-            // if any other item picked up
+
             else
             {
-              bool result = inventoryManager.AddItem(item);
+                Item item = col.gameObject.GetComponent<ItemInfo>().item;
+                bool result = inventoryManager.AddItem(item);
                 // if there are available inventory slots
                 if (result)
                 {
-                  Debug.Log("Item added to inventory");
+                    Debug.Log("Item added to inventory");
                     // hide from scene
                     col.gameObject.SetActive(false);
                 }
             }
-
         }
     }
 }
