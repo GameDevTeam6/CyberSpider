ffb2abfadcc68a65a0354edfc2634ded025738dc
MariyaShavshy <mariyashavshy@protonmail.com>
2023-09-10 20:47:22 -0700

    added mute button functionality to ui

---
+++ Assets/_Scripts/MuteAudioButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteAudioButton : MonoBehaviour
+{
+    [SerializeField] Button mute_btn;
+    public void MuteAudio(bool muted)
+    {
+        Debug.Log("Muted");
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = 1;
+            var colors = mute_btn.colors;
+            colors.selectedColor = Color.white;
+            mute_btn.colors = colors;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
