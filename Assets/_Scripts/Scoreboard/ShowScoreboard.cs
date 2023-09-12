deb2c54c7c6a1da7069a993820b9bdf1f4cfda18
Chey <chenanagan0830@gmail.com>
2023-09-12 21:11:06 +0200

    Added scoreboard functionality, game over trigger

---
+++ Assets/_Scripts/Scoreboard/ShowScoreboard.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ShowScoreboard : MonoBehaviour {
+
+    private Transform scoreShowBarTemplate;
+
+    private void Awake() {
+        // Get Template of ScoreBar
+        scoreShowBarTemplate = transform.Find("scoreShowBarTemplate");
+    }
+
+    private void Start() {
+        // Get And Check All ScoreBoard Data Count
+        int indexCount = ScoreboardManager.Instance.GetAllScoreBoardData().Count;
+        if(indexCount < 0) return;
+
+        // Sorting Algoritm
+        for (int i = 0; i < ScoreboardManager.Instance.GetAllScoreBoardData().Count; i++) {
+            for (int j = 0; j < ScoreboardManager.Instance.GetAllScoreBoardData().Count; j++) {
+                if(ScoreboardManager.Instance.GetAllScoreBoardData()[j].score < ScoreboardManager.Instance.GetAllScoreBoardData()[i].score) {
+                   ScoreboardData higherData = ScoreboardManager.Instance.GetAllScoreBoardData()[i];
+                   ScoreboardManager.Instance.GetAllScoreBoardData()[i] = ScoreboardManager.Instance.GetAllScoreBoardData()[j];
+                   ScoreboardManager.Instance.GetAllScoreBoardData()[j] = higherData; 
+                }
+            }
+        }
+
+        // Make a Copy of ScoreBar According to the Data
+        for (int i = 0; i < ScoreboardManager.Instance.GetAllScoreBoardData().Count; i++) {
+            if(i > 9) return;
+            Transform cloneBar = Instantiate(scoreShowBarTemplate);
+            cloneBar.parent = transform;
+            cloneBar.localScale = new Vector3(1, 1, 1);
+            ScoreboardData data = ScoreboardManager.Instance.GetAllScoreBoardData()[i];
+            cloneBar.GetComponentInChildren<TMP_Text>().text = $"({i + 1})   {data.name}   {data.score}";
+            cloneBar.gameObject.SetActive(true);
+        }
+    }
+}
