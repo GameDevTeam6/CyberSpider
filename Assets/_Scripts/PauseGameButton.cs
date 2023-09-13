3939f3f0f6dec58552f37a083abb57af5974c623
MariyaShavshy <mariyashavshy@protonmail.com>
2023-09-13 14:26:51 -0700

    added pause play button functionality

--- Assets/_Scripts/PauseGameButton.cs
+++ Assets/_Scripts/PauseGameButton.cs
@@ -11,55 +11,41 @@ public class PauseGameButton : MonoBehaviour
     // change between images, depending on button state
     public Sprite pause_img;
     public Sprite play_img;
-    public void MuteAudio(bool paused)
-    {
-        Debug.Log("Paused");
-        if (AudioListener.volume == 0)
-        {
-            // unmute audio
-            AudioListener.volume = 1;
 
-            // update button appearance
-            var colors = pause_btn.colors;
-            colors.selectedColor = Color.white;
-            pause_btn.colors = colors;
-            Change2MuteImage();
+    private bool is_paused;
+
+    void Start()
+    {
+        is_paused = false;
+    }
+
+    public void PauseButton(bool paused)
+    {      
+        if (is_paused)
+        {
+            Debug.Log("Game Paused");
+            // unmute audio
+            ResumeGame();
+            is_paused = false;
+            AudioListener.pause = false;
         }
         else
         {
-            // mute audio           
-            AudioListener.volume = 0;
-
-            // update button appearance       
-            var colors = pause_btn.colors;
-            colors.selectedColor = Color.red;
-            pause_btn.colors = colors;
-            Change2UnmuteImage();
+            Debug.Log("Game Resumed");
+            // pause game;           
+            PauseGame();
+            is_paused = true;
+            AudioListener.pause = true;
         }
     }
 
-    public void Change2UnmuteImage()
+    public void PauseGame()
     {
-        // change to unmute button icon
-        pause_btn.image.sprite = play_img;
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
     }
 
-    public void Change2MuteImage()
-    {
-        // change to mute button icon
-        pause_btn.image.sprite = pause_img;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
-
